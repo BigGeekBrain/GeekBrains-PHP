be3502297bc44c990c3d7c26e1e679d22cb4d303
Aleksey Ardalin <aleksey@ardalin.com>
2017-05-30 17:04:03 +0300

    phpL2T5

---
+++ Aleksey_Ardalin/lesson-2/homework/standard_homework/lesson2task5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+
+<body>
+  <!--
+5.
+вывести текущий год в подвале при помощи встроенных функций PHP.
+-->
+  <?php echo 'Текущий год: ' . date('Y'); ?>
+</body>
+
+</html>
