a22994fefc23dc2c2947639cf151d6b60825c57b
Damir92 <home_damir92@mail.ru>
2017-06-05 17:30:50 +0300

    5.06

---
+++ Damir_Masalimov/lesson-2/homework/advanced_homework/third.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Third exercise</title>
+</head>
+<body>
+<div>
+	<h2>Задание 3 (advanced)</h2>
+	<p>Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.</p>
+</div>
+	<?php
+	$operandA=5;
+	$operandB=10;
+		function summ($operand1, $operand2)
+		{
+			return $operand1 + $operand2;
+		}
+		function diff($operand1, $operand2)
+		{
+			return $operand1 - $operand2;
+		}
+		function mult($operand1, $operand2)
+		{
+			return $operand1 * $operand2;
+		}
+		function div($operand1, $operand2)
+		{
+			return $operand1 / $operand2;
+		}
+
+		echo 'Операнд 1: ' . $operandA . '<br>';
+		echo 'Операнд 2: ' . $operandB . '<br><br>';
+
+		$resultSumm=summ($operandA, $operandB);
+		$resultDiff=diff($operandA, $operandB);
+		$resultMult=mult($operandA, $operandB);
+		$resultDiv=div($operandA, $operandB);
+		echo 'Сумма: ' . $resultSumm . '<br>';
+		echo 'Разность: ' . $resultDiff . '<br>';
+		echo 'Умножение: ' . $resultMult . '<br>';
+		echo 'Деление: ' . $resultDiv . '<br>';
+	?>
+</body>
+</html>
