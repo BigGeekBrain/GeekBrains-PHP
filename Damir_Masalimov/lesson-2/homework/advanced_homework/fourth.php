64cc4c8a5e0e64d5d656e0497e7c5e3e8dd2126b
Damir92 <home_damir92@mail.ru>
2017-06-06 14:34:30 +0300

    6 июня

--- Damir_Masalimov/lesson-2/homework/advanced_homework/fourth.php
+++ Damir_Masalimov/lesson-2/homework/advanced_homework/fourth.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Third exercise</title>
+	<title>Fourth exercise</title>
 </head>
 <body>
 <div>
-	<h2>Задание 3 (advanced)</h2>
-	<p>Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.</p>
+	<h2>Задание 4 (advanced)</h2>
+	<p style="width: 50%">Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation), где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).</p>
 </div>
 	<?php
 	$operandA=5;
 	$operandB=10;
+
 		function summ($operand1, $operand2)
 		{
 			return $operand1 + $operand2;
@@ -28,14 +29,31 @@
 		{
 			return $operand1 / $operand2;
 		}
+		function mathOperation($arg1, $arg2, $operation)
+		{
+			switch ( $operation ) {
+				case '+':
+					return summ($arg1, $arg2);
+					break;
+				case '-':
+					return diff($arg1, $arg2);
+					break;
+				case '*':
+					return mult($arg1, $arg2);
+					break;
+				case '/':
+					return div($arg1, $arg2);
+					break;
+			}
+		}
 
 		echo 'Операнд 1: ' . $operandA . '<br>';
 		echo 'Операнд 2: ' . $operandB . '<br><br>';
 
-		$resultSumm=summ($operandA, $operandB);
-		$resultDiff=diff($operandA, $operandB);
-		$resultMult=mult($operandA, $operandB);
-		$resultDiv=div($operandA, $operandB);
+		$resultSumm=mathOperation($operandA, $operandB, '+');
+		$resultDiff=mathOperation($operandA, $operandB, '-');
+		$resultMult=mathOperation($operandA, $operandB, '*');
+		$resultDiv=mathOperation($operandA, $operandB, '/');
 		echo 'Сумма: ' . $resultSumm . '<br>';
 		echo 'Разность: ' . $resultDiff . '<br>';
 		echo 'Умножение: ' . $resultMult . '<br>';
