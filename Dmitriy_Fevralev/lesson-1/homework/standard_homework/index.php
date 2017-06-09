508e862c7bed72cb363ea5fa3ef5389b5f83fbd3
jicool91 <jicool912@gmail.com>
2017-06-09 14:52:55 +0300

    hello

---
+++ Dmitriy_Fevralev/lesson-1/homework/standard_homework/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<script>
+
+</script>
+
+	<?php
+
+	echo 'HelloWorld!';
+		
+
+	?>
+</body>
+</html>
