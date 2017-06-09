6bf2a30e7716423181fb5b4dbb7174a6e5e3d7f4
Mikhail <m.chuenkov@gmail.com>
2017-06-09 18:03:34 +0300

    2 задание

---
+++ Михаил_Чуенков/lesson-2/homework/standard_homework/switch_0-15.php
@@ -0,0 +1,134 @@
+<?php
+$a = 7;
+
+switch ($a) {
+    case 0:
+        echo 'a равно 0 <br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 1:
+        echo 'a равно 1<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 2:
+        echo 'a равно 2<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+       break;
+
+    case 3:
+        echo 'a равно 3<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 4:
+        echo 'a равно 4<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 5:
+        echo 'a равно 5<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 6:
+        echo 'a равно 6<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 7:
+        echo 'a равно 7<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 8:
+        echo 'a равно 8<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 9:
+        echo 'a равно 9<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 10:
+        echo 'a равно 10<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 11:
+        echo 'a равно 11<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 12:
+        echo 'a равно 12<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 13:
+        echo 'a равно 13<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+
+    case 14:
+        echo 'a равно 14<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+        
+    case 15:
+        echo 'a равно 15<br>';
+        while ( $a <= 15){
+        echo (" $a ");
+        $a++;
+        }
+        break;
+}
+
+?>
